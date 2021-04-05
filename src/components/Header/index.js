33c0859a5afd4a82e504e599270a758bce452c8b
Martin M <martin7mondaca@gmail.com>
2021-04-04 20:43:51 -0700

    created header for application and adding styling to center title

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css"
+
+function Row() {
+    return (
+        <header className="">
+            <nav className="navbar navbar-dark bg-dark text-center row my-auto">
+                <div className="col">
+                    <span class="app-title navbar-brand mb-0 h1">Employee Directory</span>
+                </div>
+            </nav>
+        </header>)
+}
+
+export default Row;

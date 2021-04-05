ca5c68c7bdb616ab0beb6fd872d891c2041cfa25
Martin M <martin7mondaca@gmail.com>
2021-04-04 23:58:30 -0700

    changed button name to 'Filter' instead of 'Search'

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -7,10 +7,10 @@ function Filter() {
             <div className="col">
                 <form className="form-inline">
                     <div className="form-group mx-sm-3 mb-2">
-                        <label for="searchBy" className="sr-only">Search by first name:</label>
-                        <input type="name" className="form-control" id="searchBy" placeholder="first name" />
+                        <label for="firstNameFilter" className="sr-only">Search by first name:</label>
+                        <input type="name" className="form-control" id="firstNameFilter" placeholder="first name" />
                     </div>
-                    <button type="submit" class="btn btn-primary mb-2">Search</button>
+                    <button type="submit" class="btn btn-primary mb-2">Filter</button>
 
                 </form>
             </div>
